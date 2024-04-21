ccf43e9cbc3f67119850715895d507a68e3066cc
light <light@lights-MacBook-Air.local>
2024-04-20 19:29:34 -0500

    first commit

---
+++ push_to_database.py
@@ -0,0 +1,40 @@
+from scrape import *
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+functions = [league_table,top_scorers,detail_top,player_table,all_time_table,all_time_winner_club,top_scorers_seasons,goals_per_season]
+
+
+#conn_string = os.getenv('CONN_STRING')
+#db = create_engine(conn_string)
+
+url = URL.create(
+    drivername="postgresql",
+    username=os.getenv('USER'),
+    password=os.getenv('PASSWORD'),
+    host=os.getenv('HOST'),
+    port=os.getenv('PORT'),
+    database="postgres"
+)
+
+db = create_engine(url)
+conn = db.connect() 
+
+
+
+
+
+for fun in functions:
+    function_name = fun.__name__
+    result_df = fun()  # Call the function to get the DataFrame
+    result_df.to_sql(function_name, con=conn, if_exists='replace', index=False)
+    print(f'Pushed data for {function_name}')
+
+# Close the database connection
+conn.close()
